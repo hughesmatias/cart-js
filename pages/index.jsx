3e921e853d25dcce7137188ed30566ec6a3e66c4
Matias Perez Hughes <hughesmatias@gmail.com>
2020-04-12 19:36:25 -0300

    Layout component

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import Link from 'next/link';
-import Header from '../components/Header';
+import Layout from '../components/Layout';
 
 export default class Index extends Component {
   render() {
     return (
       <div>
-        <Header />
-        
-        <Link href="/product">
-        <a>Product 1</a>
-        </Link>
-        <Link href="/product">
-          <a>Product 2</a>
-        </Link>
+        <Layout>
+          <Link href="/product?name=vaso">
+          <a>Product 1</a>
+          </Link>
+          <Link href="/products/[name]" as={`/products/Product 2`}>
+            <a>Product 2</a>
+          </Link>
+        </Layout>
       </div>
     )
   }
