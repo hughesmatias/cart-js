03bf7f0701ab358b761278fa513fc55fab81aa64
Matias Perez Hughes <hughesmatias@gmail.com>
2020-04-12 19:37:35 -0300

    login and product in layout

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
+import Layout from '../components/Layout';
 
 export default class Login extends Component {
   render() {
     return (
       <div>
-        <input type="text" name="user"/>
-        <input type="password" name="pass"/>
-        <button type="submit">Login</button>
+        <Layout>
+          <input type="text" name="user"/>
+          <input type="password" name="pass"/>
+          <button type="submit">Login</button>
+        </Layout>
       </div>
     )
   }
--- pages/product.jsx
+++ pages/product.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
 
-const product = props => (
-  <div>
-    Product name: this name...
-  </div>
-)
+const product = props => {
+  const router = useRouter()
+
+  return  (
+    <div>
+      <Layout>
+        Product name: {router.query.name}
+      </Layout>
+    </div>
+  )
+}
 
 export default product;
